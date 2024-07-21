65ee841466af717e8dc5acc6f5a41e75fbc02279
Sandro Simperl <sandrosimperl@outlook.de>
2024-07-21 02:19:39 +0200

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module eu.bluefogdev.maskoverride {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
     requires java.prefs;
     requires java.logging;
-
 
     opens eu.bluefogdev.maskoverride to javafx.fxml;
     exports eu.bluefogdev.maskoverride;
